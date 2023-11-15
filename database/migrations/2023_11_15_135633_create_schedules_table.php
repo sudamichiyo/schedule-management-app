d3be61d51cc740e1482df7da1fcfd51b6da83125
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-11-16 00:18:56 +0900

    [fix] マイグレーションファイルの修正

--- database/migrations/2023_11_15_135633_create_schedules_table.php
+++ database/migrations/2023_11_15_135633_create_schedules_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('schedule', function (Blueprint $table) {
+        Schema::create('schedules', function (Blueprint $table) {
             $table->increments('id');
             $table->text('title');
             $table->datetime('begin');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('schedule');
+        Schema::dropIfExists('schedules');
     }
 };
