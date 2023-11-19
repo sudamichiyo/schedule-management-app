152bc34b7aaa7bfdc822514fdfe1625760b63a3a
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-11-19 17:57:13 +0900

    [add] 入力チェックのエラーメッセージの表示を追加

--- resources/views/schedule/form.blade.php
+++ resources/views/schedule/form.blade.php
@@ -1,36 +1,37 @@
 <body>
-      <form action="/schedule/{{ $schedule->id }}" method="post">
-        @if($target == 'update')
-        @method('put')
-        @endif
-        @csrf
-        <label>
-            タイトル : <input type="text" name="title" value="{{ $schedule->title }}">
-        </label>
-        <br>
-        <label>
-            開始日時 : <input type="datetime-local" name="begin" value="{{ $schedule->begin }}">
-        </label>
-        <br>
-        <label>
-            終了日時 : <input type="datetime-local" name="end" value="{{ $schedule->end }}">
-        </label>
-        <br>
-        <label>
-            場所 : <input type="text" name="place" value="{{ $schedule->place }}">
-        </label>
-        <br>
-        <label>
-            内容 : <textarea cols="20" rows="5" name="content">{{ $schedule->content }}</textarea>
-        </label>
-        @if($target == 'store')
-            <button type="submit">登録</button>
-        @else
-            <button type="submit">保存</button>
-        @endif
-        
-      </form>
+    @include('schedule/message')
+    <form action="/schedule/{{ $schedule->id }}" method="post">
+    @if($target == 'update')
+    @method('put')
+    @endif
+    @csrf
+    <label>
+        タイトル : <input type="text" name="title" value="{{ $schedule->title }}">
+    </label>
+    <br>
+    <label>
+        開始日時 : <input type="datetime-local" name="begin" value="{{ $schedule->begin }}">
+    </label>
+    <br>
+    <label>
+        終了日時 : <input type="datetime-local" name="end" value="{{ $schedule->end }}">
+    </label>
+    <br>
+    <label>
+        場所 : <input type="text" name="place" value="{{ $schedule->place }}">
+    </label>
+    <br>
+    <label>
+        内容 : <textarea cols="20" rows="5" name="content">{{ $schedule->content }}</textarea>
+    </label>
+    @if($target == 'store')
+        <button type="submit">登録</button>
+    @else
+        <button type="submit">保存</button>
+    @endif
+    
+    </form>
 
-      <button><a href="/schedule/{{ $schedule->id }}">キャンセル</a></button>
+    <button><a href="/schedule/{{ $schedule->id }}">キャンセル</a></button>
 </body>
     
